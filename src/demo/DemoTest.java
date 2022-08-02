d801d8d06122331a7c24a4c8d3c48d10f13e165f
brand <brand@10.0.0.237>
2022-08-01 20:09:23 -0400

    Initial commit

---
+++ src/demo/DemoTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("John Cena");
+
+	}
+
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public static void divide(int a, int b) {
+		System.out.println(a / b);
+	}
+
+}
